637ad7e1dc7b30126e4acb5f9e1a107cae082665
Tiago Do Couto <cs.couto@outlook.com>
2019-01-25 10:48:58 -0600

    project

---
+++ UploadKit/UploadKit/UploadKit.h
@@ -0,0 +1,19 @@
+//
+//  UploadKit.h
+//  UploadKit
+//
+//  Created by COUTO, TIAGO [AG-Contractor/1000] on 1/25/19.
+//  Copyright © 2019 Couto Code. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UploadKit.
+FOUNDATION_EXPORT double UploadKitVersionNumber;
+
+//! Project version string for UploadKit.
+FOUNDATION_EXPORT const unsigned char UploadKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UploadKit/PublicHeader.h>
+
+
